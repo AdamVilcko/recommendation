ee10d18fe153cb2454bb50300bda9d718e49e4af
Adam <adam.vilcko@hotmail.com>
2014-11-04 19:33:29 +0100

    Build changes.

---
+++ recommendation-web/src/main/java/cz/ucl/config/SpringContextInitializer.java
@@ -0,0 +1,23 @@
+package cz.ucl.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.web.context.ConfigurableWebApplicationContext;
+
+/**
+ * Custom Spring application context initializer to be able to extend some Spring core features.
+ *
+ * @author Adam Vilcko
+ */
+public class SpringContextInitializer implements ApplicationContextInitializer<ConfigurableWebApplicationContext> {
+
+	private static final Logger LOG = LoggerFactory.getLogger(SpringContextInitializer.class);
+
+    @Override
+    public void initialize(ConfigurableWebApplicationContext applicationContext) {
+        // place additional configuration here
+    	LOG.info("Initalizing the application...");
+    }
+
+}
