b1d823df3531542e0142c02ab9de6e5e3cb0d7d6
Adam <adam.vilcko@hotmail.com>
2014-11-17 15:29:40 +0100

    First working draft. It shows only simple Thymeleaf (html) page.

--- recommendation-web/src/main/java/cz/ucl/web/controller/SiteController.java
+++ recommendation-web/src/main/java/cz/ucl/web/controller/SiteController.java
@@ -3,12 +3,14 @@ package cz.ucl.web.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class SiteController {
 
-	@RequestMapping("/recom")
-	public String getContent(Model model) {
+	@RequestMapping(method=RequestMethod.GET)
+	public String index(Model model) {
 		model.addAttribute("name", "Adam");
 		return "index";
 	}
